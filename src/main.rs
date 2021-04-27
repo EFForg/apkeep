24781948cb0011005836effe5ec01ff8490c2205
William Budington <bill@eff.org>
2021-04-27 14:50:59 -0700

    Build-out and error handling

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use serde_json::json;
 use std::env;
 use std::fs;
 use std::path::Path;
+use std::time::Duration;
 use thirtyfour::prelude::*;
 
 fn help_message_and_exit(program_name: &str, status_code: i32) -> String {
@@ -31,32 +32,73 @@ async fn fetch_ar_list() -> Result<Vec<String>, Box<dyn std::error::Error>> {
 
 async fn download_ar_apps(app_ids: Vec<String>, outpath: &str) -> WebDriverResult<()> {
     let fetches = futures_util::stream::iter(
-	app_ids.into_iter().map(|app_id| {
+        app_ids.into_iter().map(|app_id| {
             async move {
-                println!("Downloading {}...", app_id);
-                let app_url = format!("https://apkpure.com/a/{}/download?from=details", app_id);
-                let mut caps = DesiredCapabilities::chrome();
-                let filepath = format!("{}", Path::new(outpath).join(app_id).to_str().unwrap());
-                let prefs = json!({
-                    "download.default_directory": filepath
-                });
-                caps.add_chrome_option("prefs", prefs).unwrap();
-
-                let driver = WebDriver::new("http://localhost:4444", &caps).await.unwrap();
-                driver.get(app_url).await.unwrap();
-                let elem_result = driver.find_element(By::Css("span.file")).await.unwrap();
-                let re = Regex::new(r" \([0-9.]+ MB\)$").unwrap();
-                let new_filename = elem_result.text().await.unwrap();
-                let new_filename = re.replace(&new_filename, "").into_owned();
-                let paths = fs::read_dir(&filepath).unwrap();
-                let old_filename = paths.filter_map(|path| path.ok()).collect::<Vec<fs::DirEntry>>()[0].file_name();
-                fs::rename(Path::new(&filepath).join(old_filename), Path::new(&filepath).join(new_filename)).unwrap();
+                match download_single_app(&app_id, outpath).await {
+                    Ok(res_tuple) => futures_util::future::ready(Some(res_tuple)),
+                    Err(_) => {
+                        println!("An error has occurred attempting to download {}.  Retry #1...", app_id);
+                        match download_single_app(&app_id, outpath).await {
+                            Ok(res_tuple) => futures_util::future::ready(Some(res_tuple)),
+                            Err(_) => {
+                                println!("An error has occurred attempting to download {}.  Retry #2...", app_id);
+                                match download_single_app(&app_id, outpath).await {
+                                    Ok(res_tuple) => futures_util::future::ready(Some(res_tuple)),
+                                    Err(_) => {
+                                        println!("An error has occurred attempting to download {}.  Aborting.", app_id);
+                                        futures_util::future::ready(None)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
         })
-    ).buffer_unordered(4).collect::<Vec<()>>();
+    //).buffer_unordered(4).filter_map(|i| i).collect::<Vec<(String, String, String)>>();
+    ).buffer_unordered(4).filter_map(|i| i).collect::<Vec<(String, String, String)>>();
     println!("Waiting...");
-    fetches.await;
+    let results = fetches.await;
+    for move_file in results {
+        if let Ok(paths) = fs::read_dir(&move_file.0) {
+            let dir_list = paths.filter_map(|path| path.ok()).collect::<Vec<fs::DirEntry>>();
+            if dir_list.len() > 0 {
+                println!("Saving {}...", move_file.2);
+                let old_filename = dir_list[0].file_name();
+                fs::rename(Path::new(&move_file.0).join(old_filename), Path::new(&move_file.0).join(move_file.1)).unwrap();
+            } else {
+                println!("Could not save {}...", move_file.2);
+            }
+        } else {
+            println!("Could not save {}...", move_file.2);
+        }
+    }
     Ok(())
+}
+
+async fn download_single_app(app_id: &str, outpath: &str) -> WebDriverResult<(String, String, String)> {
+    println!("Downloading {}...", app_id);
+    let app_url = format!("https://apkpure.com/a/{}/download?from=details", app_id);
+    let mut caps = DesiredCapabilities::chrome();
+    let filepath = format!("{}", Path::new(outpath).join(app_id.clone()).to_str().unwrap());
+    let prefs = json!({
+        "download.default_directory": filepath
+    });
+    caps.add_chrome_option("prefs", prefs).unwrap();
+
+    let driver = match WebDriver::new("http://localhost:4444", &caps).await {
+        Ok(driver) => driver,
+        Err(_) => panic!("chromedriver must be running on port 4444")
+    };
+    let delay = Duration::new(10, 0);
+    driver.set_implicit_wait_timeout(delay).await?;
+    driver.get(app_url).await?;
+    let elem_result = driver.find_element(By::Css("span.file")).await?;
+    let re = Regex::new(r" \([0-9.]+ MB\)$").unwrap();
+
+    let new_filename = elem_result.text().await?;
+    let new_filename = re.replace(&new_filename, "").into_owned();
+    Ok((filepath, new_filename, String::from(app_id)))
 }
 
 #[tokio::main]
